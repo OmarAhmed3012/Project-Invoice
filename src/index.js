e7bf066f0d28119f2b3b6cbc79bd2949a669b666
Karim <karimomakram11@gmail.com>
2020-09-28 18:48:49 +0200

    calculate totla invoices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const expressLayout =  require('express-ejs-layouts');
-
+const cors = require('cors')
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(expressLayout);
+app.use(cors())
 app.set('view engine', 'ejs');
 
 
